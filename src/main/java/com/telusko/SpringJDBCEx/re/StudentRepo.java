e2fc010c2602d79ea431b0366664bd93b0539db7
VinceBmmrt <vincebommert75@gmail.com>
2025-05-12 17:53:54 +0200

    Creation of JDBC Project with SpringBout + Dependency added + Creation of 2 layers : Service and Repo

---
+++ src/main/java/com/telusko/SpringJDBCEx/re/StudentRepo.java
@@ -0,0 +1,19 @@
+package com.telusko.SpringJDBCEx.re;
+
+import com.telusko.SpringJDBCEx.model.Student;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Repository
+public class StudentRepo {
+    public void save(Student s) {
+        System.out.println("added");
+    }
+
+    public List<Student> findAll() {
+        List<Student> students = new ArrayList<>();
+        return students;
+    }
+}
